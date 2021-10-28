cec351ef025ebe96acee2417a2e7954af1e6802f
Nicolas Rodriguez <nico@nicoladmin.fr>
2021-10-29 00:27:11 +0200

    docs: fix CLI documentation

--- src/squarectl/cli.cr
+++ src/squarectl/cli.cr
@@ -4,13 +4,13 @@ module Squarectl
     define_version Squarectl.version
     define_help description: "Squarectl is Salt PillarStack in Crystal"
 
-    register_sub_command compose, Compose, description: "Show Squarectl information"
-    register_sub_command swarm, Swarm, description: "Run Squarectl webserver"
-    register_sub_command configs, Configs, description: "Run Squarectl webserver"
-    register_sub_command secrets, Secrets, description: "Fetch host pillars"
-    register_sub_command info, Info, description: "Fetch host pillars"
-    register_sub_command kube, Kube, description: "Fetch host pillars"
-    register_sub_command completion, Completion, description: "Fetch host pillars"
+    register_sub_command compose, Compose, description: "Run Docker Compose commands"
+    register_sub_command swarm, Swarm, description: "Run Docker Swarm commands"
+    register_sub_command configs, Configs, description: "Manage Docker Swarm Configs"
+    register_sub_command secrets, Secrets, description: "Manage Docker Swarm Secrets"
+    register_sub_command info, Info, description: "Get Squarectl info"
+    register_sub_command kube, Kube, description: "Run Kubernetes commands"
+    register_sub_command completion, Completion, description: "Install shell completion"
 
     def run
       puts help
