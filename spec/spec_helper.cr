2f70b819cd3db626d46ecdbb2b2150514da1490b
Nicolas Rodriguez <nico@nicoladmin.fr>
2021-10-29 03:52:05 +0200

    test: improve Spectator config

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,4 +1,10 @@
-require "spectator"
 require "crystal-env/spec"
+require "spectator"
+
+# See: https://gitlab.com/arctic-fox/spectator/-/wikis/Configuration
+Spectator.configure do |config|
+  config.randomize
+  config.profile
+end
 
 require "../src/squarectl"
